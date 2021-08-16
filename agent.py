1098a56905f457bb62bfe2fbba524c7565214617
lukechn99 <47294899+lukechn99@users.noreply.github.com>
2021-08-15 19:56:59 -0500

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,5 @@
+class agent:
+    def __init__(self, name, engagement, attributes):
+        self.name = name
+        self.engagement = engagement
+        self.attributes = attributes
---
+++ main.py
@@ -0,0 +1,50 @@
+from agent import agent
+from typing import List
+
+import json
+import logging
+logging.basicConfig(filename='app.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(asctime)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+FILE = "agents.json"
+
+# load agents
+"""
+CONVERSATION
+conversation should flow simply...
+
+I like ...
+That's very cool, I like ...
+
+
+"""
+
+"""
+How to fuzzy match terms?
+"""
+
+
+def load_agents() -> List[agent]:
+    agents = []
+    try:
+        with open(FILE, 'r') as f:
+            data = json.load(f)
+            for entry in list(data.keys):
+                new_agent = agent(
+                    entry, data[entry]["engagement"], data[entry]["attributes"])
+                agents.append(new_agent)
+        return agents
+    except Exception as e:
+        logger.error(str(e))
+        return []
+
+
+def converse(agents: List[agent]):
+    if not agents:
+        pass
+
+
+def main():
+    agents = load_agents()
+    converse(agents)
