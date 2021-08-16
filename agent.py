d89a15a7fbe80119b4f7851c5d4d5ab2e9e742ce
lukechn99 <47294899+lukechn99@users.noreply.github.com>
2021-08-16 16:53:24 -0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,5 +1,23 @@
+from typing import List
+
+
 class agent:
     def __init__(self, name, engagement, attributes):
         self.name = name
         self.engagement = engagement
         self.attributes = attributes
+
+    def get_attributes(self) -> List[str]:
+        return self.attributes
+
+    def add_attribute(self, attribute):
+        self.attributes.append(attribute)
+
+    def get_engagement(self):
+        return self.engagement
+
+    def modify_engagement(self, amount):
+        self.engagement += amount
+
+    def get_name(self):
+        return self.name
